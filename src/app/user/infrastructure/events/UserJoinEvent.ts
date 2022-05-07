0d403c8610d141f11a48c0ca12eff2526f376c41
HyungJu <sungkisa@naver.com>
2022-05-08 00:51:28 +0900

    chore: enforce all rules

--- src/app/user/infrastructure/events/UserJoinEvent.ts
+++ src/app/user/infrastructure/events/UserJoinEvent.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserJoinEvent {
   @OnEvent('user.joined')
-  handleOrderCreatedEvent(payload: any) {
+  handleOrderCreatedEvent(payload: any): void {
     console.log('TEST');
     console.log(payload);
   }
